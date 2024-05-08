fa31c82b1f7e1f23bbbe6983dc32271f49f79993
shivamk1998 <shivamkumarsingh1998@gmail.com>
2024-05-08 18:49:42 +0530

    Added base structure and authentication with clerk

---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  providers: [
+    {
+      domain: "https://real-goblin-29.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};

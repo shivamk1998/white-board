4874ee273767dcc4a6862fc5823368676e48a28a
shivamk1998 <shivamkumarsingh1998@gmail.com>
2024-05-09 16:13:53 +0530

    added dashboard ayout and sidebar

--- providers/convex-client-provider.tsx
+++ providers/convex-client-provider.tsx
@@ -1,23 +1,27 @@
 "use client";
 import { ReactNode } from "react";
+
+
 import { AuthLoading, Authenticated, ConvexProvider, ConvexReactClient } from "convex/react";
-import {ClerkProvider,useAuth } from "@clerk/clerk-react";
+import {ClerkProvider,useAuth } from "@clerk/nextjs";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { Loading } from "@/components/auth/loading";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
+
+
 export default function ConvexClientProvider({
   children,
 }: {
-  children: ReactNode;
+  children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}>
+    <ClerkProvider>
       <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
-        <Authenticated>
+       
         {children}
-        </Authenticated>
+       
         <AuthLoading>
           <Loading/>
         </AuthLoading>

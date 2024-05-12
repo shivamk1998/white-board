2954a3d87122bc4a9a05666c9c295857e9179b98
shivamk1998 <shivamkumarsingh1998@gmail.com>
2024-05-12 22:12:19 +0530

    updated navbar.

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,18 +1,46 @@
 "use client";
 
-import { UserButton } from "@clerk/clerk-react";
+import { OrganizationSwitcher, UserButton, useOrganization } from "@clerk/clerk-react";
+import { SearchInput } from "./search-iput";
+import { InviteButton } from "./invite-button";
 
 // import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 
-
-
 export const Navbar = () => {
+  const {organization}=useOrganization();
   return (
-    <div className="flex items-center gap-x-4 p-5 bg-green-500">
-      <div className="hidden lg:flex lg:flex-1"></div>
-      <div><UserButton/></div>
- 
+    <div className="flex items-center gap-x-4 p-5">
+      <div className="hidden lg:flex lg:flex-1">
+        <SearchInput />
+      </div>
+      <div className="block lg:hidden flex-1">
+        <OrganizationSwitcher
+          hidePersonal
+          appearance={{
+            elements: {
+              rootBox: {
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "100%",
+                maxWidth:"376px"
+              },
+              organizationSwitcherTrigger: {
+                padding: "6px",
+                width: "100%",
+                borderRadius: "8px",
+                border: "1px solid #E5E7EB",
+                justifyContent: "space-between",
+                backgroundColor: "white",
+              },
+            },
+          }}
+        />
+      </div>
+      { organization && (<InviteButton/>)}
+      <div>
+        <UserButton />
+      </div>
     </div>
-  )
-}
-
+  );
+};

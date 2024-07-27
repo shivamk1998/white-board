082770031a437504ed0d5690f9a41030b3b2dee2
shivamk1998 <shivamkumarsingh1998@gmail.com>
2024-07-27 23:30:37 +0530

    added card actions

--- convex/board.ts
+++ convex/board.ts
@@ -35,3 +35,43 @@ handler:async(ctx,args)=>{
   return board;
 }
 })
+
+
+export const remove = mutation({
+  args:{id: v.id("boards")},
+  handler:async(ctx,args)=>{
+    const identity = await ctx.auth.getUserIdentity();
+
+    if(!identity){
+      throw new Error("Unauthorized")
+    }
+ 
+    //TODO : Later check to delete favourite relation as well
+    await ctx.db.delete(args.id);
+  }
+});
+
+export const update = mutation({args:{id: v.id("boards"), title: v.string()}, handler: async(ctx, args)=>{
+
+  const identity = await ctx.auth.getUserIdentity();
+
+  if(!identity){
+    throw new Error("Unauthorized")
+  }
+
+  const title = args.title.trim();
+
+  if(!title){
+    throw new Error("Title is required")
+  }
+
+  if(title.length > 60){
+    throw new Error("Title cannot be longer than 60 characters")
+  }
+
+  const board = await ctx.db.patch(args.id,{
+    title: args.title,
+  })
+
+  return board;
+}})
